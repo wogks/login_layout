daa35dc0344a575825db8303168d5a668ac518e9
wogks <wogks@wogksui-MacBookPro.local>
2022-10-24 16:00:56 +0900

    Feat: formfield 생성

--- lib/presentation/auth_screen/auth_screen.dart
+++ lib/presentation/auth_screen/auth_screen.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'dart:developer';
 
-import '../../util/screen_provider.dart';
+import 'package:flutter/material.dart';
 
 class AuthScreen extends Page {
   static const pageName = 'AuthScreen';
@@ -22,21 +21,92 @@ class AuthWidget extends StatefulWidget {
 }
 
 class _AuthWidgetState extends State<AuthWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final OutlineInputBorder _border = OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent, width: 0),
+      borderRadius: BorderRadius.circular(8));
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: InkWell(
-        onTap: () {
-          Provider.of<ScreenNotifier>(context, listen: false).goToMain();
-        },
-        child: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/image.gif'),
-          )),
+      child: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/image.gif'),
+        )),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                padding: const EdgeInsets.all(16),
+                children: [
+                  const SizedBox(height: 16),
+                  CircleAvatar(
+                      backgroundColor: Colors.white54,
+                      radius: 36,
+                      child: Image.asset('assets/iconn.png')),
+                  const SizedBox(height: 16),
+                  _buildTextFormField('Email Address', _emailController),
+                  const SizedBox(height: 8),
+                  _buildTextFormField('Passord', _passwordController),
+                  const SizedBox(height: 8),
+                  _buildTextFormField(
+                      'Confirm Password', _confirmPasswordController),
+                  const SizedBox(height: 16),
+                  TextButton(
+                    
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                       log('good');
+                        }
+                      },
+                      style: TextButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          backgroundColor: Colors.white54),
+                      child: const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text('Login',style: TextStyle(color: Colors.black),),
+                      )),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  TextFormField _buildTextFormField(
+      String labelText, TextEditingController controller) {
+    return TextFormField(
+      validator: (text) {
+        if (text == null || text.isEmpty) {
+          return '입력창이 비어있어요!';
+        }
+        return null; //null이 정상값
+      },
+      controller: controller,
+      style: const TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        errorStyle: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        errorBorder: _border.copyWith(borderSide: const BorderSide(color: Colors.black, width: 3)),
+          filled: true,
+          fillColor: Colors.black45,
+          labelText: labelText,
+          border: _border,
+          enabledBorder: _border,
+          focusedBorder: _border,
+          labelStyle: const TextStyle(color: Colors.white)),
     );
   }
 }
